47e8b2195ad42e24074528bf8a1787fd3cc03bf8
ismail hassan <ismailshariff350@gmail.com>
2024-11-20 20:16:01 -0600

    desert added to my favourite food

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -66,6 +66,9 @@ console.log('Added an animal to end,', animalArray);
 //      a dessert that you love.
 //      Similar to above, add the `dessert` to the end of your `favoriteFoods`
 //      array. (How can you be 100% certain this worked? 🤔)
+let dessert = 'samosa';
+favoriteFoods.push(dessert);
+console.log('added dessertto end,', favoriteFoods);
 
 // Example: Add to the beginning of an array using the Array.unshift method.
 let animalToUnshift = 'walrus';
@@ -75,6 +78,7 @@ console.log(`Added an animal to beginning: ${animalArray}`);
 // 4.b. TODO: Create a variable `entree` and assign it a string value of
 //      an entree that you enjoy.
 //      Add the `entree` to the beginning of your `favoriteFoods` array.
+ 
 
 // Example: Remove the last animal by using the Array.pop method.
 let removedAnimal = animalArray.pop();
